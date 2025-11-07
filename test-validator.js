e0acd26ae20d0a977046b5c578ca03b57ac87cb0
kasumi-q00p <kasumi.ioui@gmail.com>
2025-11-07 13:07:15 +0900

    修正

--- test-validator.js
+++
@@ -1,9 +0,0 @@
-// 簡単なテスト
-const testChars = ['か', 'う', 'し', 'と', 'あ', 'い', 'ア', 'イ', 'A', 'B', '1', '2'];
-
-// ひらがなのパターンをテスト
-const hiraganaPattern = /[\u3040-\u309F]/;
-
-testChars.forEach(char => {
-  console.log(`文字: ${char}, ひらがな判定: ${hiraganaPattern.test(char)}, Unicode: ${char.charCodeAt(0).toString(16)}`);
-});
